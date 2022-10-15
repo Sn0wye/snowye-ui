56a4fb48b38aa9959d7d3e73b0a172055ec3a4f3
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-15 13:44:26 -0300

    feat: add text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Text, TextProps } from '@sn0wye-ui/react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum corporis ea officia. Tempore neque vero provident eius officia id necessitatibus.'
+  }
+} as Meta<TextProps>;
+
+export const Primary: StoryObj<TextProps> = {};
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'StrongText',
+    as: 'strong',
+  }
+};
+
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,1 +1,3 @@
 export * from './components/Box';
+export * from './components/Text';
+
