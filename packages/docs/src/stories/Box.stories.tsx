5c600f6c34bac1cdca40ba9d53c1fb8e80c0dd51
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-15 13:44:36 -0300

    refactor: use text component instead of span

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,15 +1,11 @@
-import { Box, BoxProps } from '@sn0wye-ui/react';
+import { Box, BoxProps, Text } from '@sn0wye-ui/react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 export default {
   title: 'Surfaces/Box',
   component: Box,
   args: {
-    children: (
-      <>
-        <span>Teste</span>
-      </>
-    )
+    children: <Text>This is a box component</Text>
   }
 } as Meta<BoxProps>;
 
