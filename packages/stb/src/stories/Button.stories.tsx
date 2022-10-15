d92601e8cf8d138c242d41695f2f48bdb5a84380
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-14 23:47:03 -0300

    convert to yarn

---
+++ packages/stb/src/stories/Button.stories.tsx
@@ -0,0 +1,10 @@
+import { Button, ButtonProps } from '@sn0wye-ui/react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Components/Button',
+  component: Button,
+  
+} as Meta<ButtonProps>;
+
+export const Default: StoryObj<ButtonProps> = {}
