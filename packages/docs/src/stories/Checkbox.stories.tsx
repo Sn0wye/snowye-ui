fd77a9448058ecf52570ecb3295c4e757751156e
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-16 13:52:05 -0300

    feat: add checkbox component

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,30 @@
+import { Box, Checkbox, CheckboxProps, Text } from '@sn0wye-ui/react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    Story => (
+      <Box
+        as='label'
+        css={{
+          display: 'flex',
+          gap: '$2'
+        }}
+      >
+        {Story()}
+        <Text size='sm'>I accept the terms.</Text>
+      </Box>
+    )
+  ]
+} as Meta<CheckboxProps>;
+
+export const Default: StoryObj<CheckboxProps> = {};
+
+export const Disabled: StoryObj<CheckboxProps> = {
+  args: {
+    disabled: true
+  }
+};
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,7 @@
 export * from './components/Avatar';
 export * from './components/Box';
 export * from './components/Button';
+export * from './components/Checkbox';
 export * from './components/Heading';
 export * from './components/Text';
 export * from './components/TextArea';
