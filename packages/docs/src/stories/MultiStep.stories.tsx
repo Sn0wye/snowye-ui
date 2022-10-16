8e2383d7378ee495b4276ea5c9090430b1fe7d5c
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-16 14:14:23 -0300

    feat: add MultiStep component

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,30 @@
+import { Box, MultiStep, MultiStepProps } from '@sn0wye-ui/react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Form/MultiStep',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1
+  },
+  decorators: [
+    Story => (
+      <Box
+        css={{
+          width: '50%'
+        }}
+      >
+        {Story()}
+      </Box>
+    )
+  ]
+} as Meta<MultiStepProps>;
+
+export const Default: StoryObj<MultiStepProps> = {};
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4
+  }
+};
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,6 +3,7 @@ export * from './components/Box';
 export * from './components/Button';
 export * from './components/Checkbox';
 export * from './components/Heading';
+export * from './components/MultiStep';
 export * from './components/Text';
 export * from './components/TextArea';
 export * from './components/TextInput';
