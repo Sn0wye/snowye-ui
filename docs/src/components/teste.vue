e99063649220e1e8edf114773fd2642783b4a9da
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-02 19:10:17 -0300

    feat/tooltip (#1)
    * chore: docs
    
    * deps: add radix-ui tooltip
    
    * feat: add tooltip
    
    * feat: add tooltip
    
    * feat: add tooltip story
    
    * fix: add displayName

---
+++ docs/src/components/teste.vue
@@ -0,0 +1,20 @@
+
+<template>
+  <div id="app">
+      <my-dashboard/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Button } from "@snowye-ui/react";
+import { Component, Vue } from 'vue-property-decorator';
+@Component({
+    components: {
+        'my-button': Button
+    }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+</style>
---
+++ packages/react/src/components/Tooltip/styles.ts
@@ -0,0 +1,49 @@
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
+
+import { keyframes, styled } from '../../styles';
+
+export const slideUpAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' }
+});
+
+export const slideRightAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(-2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' }
+});
+
+export const slideDownAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateY(-2px)' },
+  '100%': { opacity: 1, transform: 'translateY(0)' }
+});
+
+export const slideLeftAndFade = keyframes({
+  '0%': { opacity: 0, transform: 'translateX(2px)' },
+  '100%': { opacity: 1, transform: 'translateX(0)' }
+});
+
+export const StyledContent = styled(TooltipPrimitive.Content, {
+  borderRadius: '$xs',
+  padding: '$3 $4',
+  fontFamily: '$default',
+  fontSize: '$sm',
+  lineHeight: '$short',
+  color: '$gray100',
+  backgroundColor: '$gray900',
+  userSelect: 'none',
+  '@media (prefers-reduced-motion: no-preference)': {
+    animationDuration: '400ms',
+    animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
+    willChange: 'transform, opacity',
+    '&[data-state="delayed-open"]': {
+      '&[data-side="top"]': { animationName: slideDownAndFade },
+      '&[data-side="right"]': { animationName: slideLeftAndFade },
+      '&[data-side="bottom"]': { animationName: slideUpAndFade },
+      '&[data-side="left"]': { animationName: slideRightAndFade }
+    }
+  }
+});
+
+export const StyledArrow = styled(TooltipPrimitive.Arrow, {
+  fill: '$gray900'
+});
