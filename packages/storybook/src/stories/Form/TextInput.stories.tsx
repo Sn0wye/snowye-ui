252d309dfcddb56cce06cffd2875022e4e6c89d9
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-05 18:04:32 -0300

    refactor: text input component

--- packages/storybook/src/stories/Form/TextInput.stories.tsx
+++ packages/storybook/src/stories/Form/TextInput.stories.tsx
@@ -1,21 +1,19 @@
+/* eslint-disable react/jsx-key */
 import { TextInput, TextInputRootProps } from '@snowye-ui/react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Envelope, Lock } from 'phosphor-react';
+import { Envelope, Eye, EyeSlash } from 'phosphor-react';
 
 export default {
   title: 'Form/TextInput',
-  component: TextInput,
+  component: TextInput.Root,
   args: {
-    placeholder: 'Placeholder'
+    children: <TextInput.Input placeholder='Placeholder' />
   },
   argTypes: {
-    disabled: {
-      type: 'boolean',
-      defaultValue: false
-    },
-    error: {
-      type: 'boolean',
-      defaultValue: false
+    children: {
+      table: {
+        disable: true
+      }
     }
   }
 } as Meta;
@@ -24,14 +22,21 @@ export const Default: StoryObj<TextInputRootProps> = {};
 
 export const WithIcon: StoryObj<TextInputRootProps> = {
   args: {
-    leftIcon: <Envelope weight='bold' />,
-    placeholder: 'john@doe.com'
+    children: [
+      <TextInput.LeftIcon>
+        <Envelope weight='bold' />
+      </TextInput.LeftIcon>,
+      <TextInput.Input placeholder='Placeholder' />
+    ]
   }
 };
 
 export const WithPrefix: StoryObj<TextInputRootProps> = {
   args: {
-    prefix: 'call.com/'
+    children: [
+      <TextInput.Prefix>call.co/</TextInput.Prefix>,
+      <TextInput.Input placeholder='Placeholder' />
+    ]
   }
 };
 
@@ -43,13 +48,22 @@ export const WithError: StoryObj<TextInputRootProps> = {
 
 export const Disabled: StoryObj<TextInputRootProps> = {
   args: {
-    disabled: true
+    disabled: true,
+    children: <TextInput.Input placeholder='Placeholder' />
   }
 };
 
 export const Password: StoryObj<TextInputRootProps> = {
   args: {
-    type: 'password',
-    leftIcon: <Lock weight='bold' />
+    children: [
+      <TextInput.LeftIcon>
+        <Envelope weight='bold' />
+      </TextInput.LeftIcon>,
+      <TextInput.Input placeholder='Type your password...' type='password' />,
+      <TextInput.RightIcon
+        activeIcon={<Eye weight='bold' />}
+        inactiveIcon={<EyeSlash weight='bold' />}
+      />
+    ]
   }
 };
