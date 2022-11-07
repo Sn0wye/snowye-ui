6f1ce98924e15e4ec78995b92949cd7b538ab2cc
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-07 16:41:05 -0300

    fix(storybook): dialog controls

--- packages/react/src/components/Dialog/Dialog.stories.tsx
+++ packages/react/src/components/Dialog/Dialog.stories.tsx
@@ -1,15 +1,34 @@
 /* eslint-disable react/jsx-key */
 import { Button, Dialog, DialogRootProps } from '@snowye-ui/react';
-import type { Meta, StoryObj } from '@storybook/react';
+import { useArgs } from '@storybook/client-api';
+import type { Meta, Story } from '@storybook/react';
 
 export default {
   title: 'Overlay/Dialog',
   component: Dialog.Root,
   args: {
-    children: [
+    open: false
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    },
+    open: {
+      control: 'boolean'
+    }
+  }
+} as Meta;
+
+export const Default: Story<DialogRootProps> = () => {
+  const [args, updateArgs] = useArgs();
+
+  return (
+    <Dialog.Root onOpenChange={open => updateArgs({ open })} {...args}>
       <Dialog.Trigger key='1'>
         <Button>Open Dialog</Button>
-      </Dialog.Trigger>,
+      </Dialog.Trigger>
       <Dialog.Content key='2'>
         <Dialog.Title>Lorem Ipsum dolor</Dialog.Title>
         <Dialog.Description>
@@ -17,15 +36,6 @@ export default {
           erat nam at lectus urna duis convallis convallis tellus
         </Dialog.Description>
       </Dialog.Content>
-    ]
-  },
-  argTypes: {
-    children: {
-      table: {
-        disable: true
-      }
-    }
-  }
-} as Meta;
-
-export const Default: StoryObj<DialogRootProps> = {};
+    </Dialog.Root>
+  );
+};
