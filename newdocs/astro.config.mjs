e9e3fbd857d3fb290962f594619f72141976557f
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-12 00:42:09 -0300

    deps: add mdx

--- newdocs/astro.config.mjs
+++ newdocs/astro.config.mjs
@@ -3,12 +3,14 @@ import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
-    // Enable Preact to support Preact JSX components.
-    preact(),
-    // Enable React for the Algolia search component.
-    react()
-  ],
+  // Enable Preact to support Preact JSX components.
+  preact(),
+  // Enable React for the Algolia search component.
+  react(), mdx()],
   site: `http://astro.build`
 });
