9bce047bdb799b5ae5f00aa3983027671e17bde4
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-11-14 17:02:40 -0300

    style: enhance TextInput demo

--- newdocs/src/demos/TextInputDemo.tsx
+++ newdocs/src/demos/TextInputDemo.tsx
@@ -1,20 +1,35 @@
 /** @jsxImportSource react */
-import { TextInput } from '@snowye-ui/react';
-import { Envelope, Eye, EyeSlash } from 'phosphor-react';
+import { Box, TextInput } from '@snowye-ui/react';
+import { Envelope, Eye, EyeSlash, User } from 'phosphor-react';
 import { FunctionComponent } from 'react';
 
 export const TextInputDemo: FunctionComponent = () => {
   return (
-    <TextInput.Root>
-      <TextInput.LeftIcon>
-        <Envelope weight='bold' />
-      </TextInput.LeftIcon>
-      <TextInput.Input placeholder='Type your password...' type='password' />
-      <TextInput.RightIcon
-        activeIcon={<Eye weight='bold' />}
-        inactiveIcon={<EyeSlash weight='bold' />}
-      />
-    </TextInput.Root>
+    <Box
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        background: 'transparent',
+        gap: '$2'
+      }}
+    >
+      <TextInput.Root>
+        <TextInput.LeftIcon>
+          <User weight='bold' />
+        </TextInput.LeftIcon>
+        <TextInput.Input placeholder='Type your username...' />
+      </TextInput.Root>
+      <TextInput.Root>
+        <TextInput.LeftIcon>
+          <Envelope weight='bold' />
+        </TextInput.LeftIcon>
+        <TextInput.Input placeholder='Type your password...' type='password' />
+        <TextInput.RightIcon
+          activeIcon={<Eye weight='bold' />}
+          inactiveIcon={<EyeSlash weight='bold' />}
+        />
+      </TextInput.Root>
+    </Box>
   );
 };
 
